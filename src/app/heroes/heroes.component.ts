e6f34086ff71bc599eac8c0a0b61f24245a0eefa
yasharma <yashsharma205@gmail.com>
2018-01-07 23:21:10 +0530

    First commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+  	this.getHeroes();
+  }
+  heroes: Hero[];
+  // selectedHero: Hero;
+
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  // }
+  getHeroes(): void {
+  	this.heroService.getHeroes()
+  		.subscribe(heroes => this.heroes = heroes);
+  }
+}
